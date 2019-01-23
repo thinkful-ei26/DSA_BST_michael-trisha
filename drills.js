9cbc017069a4a06c2989eb9f929c361e084bfcc2
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-23 14:40:17 -0500

    class done

--- drills.js
+++ drills.js
@@ -3,7 +3,7 @@
 //hierarchy, parent-child relationship
 //parent has a max of 2 children
 class BinarySearchTree {
-  constructor(key=null, value=null, parent=null){
+  constructor(key = null, value = null, parent = null) {
     //what is the key? 
     this.key = key;
     this.value = value;
@@ -11,17 +11,21 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
-
-  insert(key, value){
-    if(this.key === null){
+  //bst.insert(3, 0)
+  /*  9
+     / \
+    3   7 
+  */
+  insert(key, value) {
+    if (this.key === null) {
       this.key = key;
       this.value = value;
     }
     //if the tree exists, start at the root and compare the key you want to insert 
     //if less go left
-    else if(key < this.key){
+    else if (key < this.key) {
       //if the left is empty, create new node
-      if(this.left === null){
+      if (this.left === null) {
         this.left = new BinarySearchTree(key, value, this); //this is parent, but what is it?
       }
       //if there's something on the left side, recursively call insert, so node is added further down the tree
@@ -31,7 +35,7 @@ class BinarySearchTree {
     }
     //if the key of the value you want to insert is greater than the current node's key, move to the right side
     else {
-      if(this.right === null){
+      if (this.right === null) {
         this.right = new BinarySearchTree(key, value, this);
       } else {
         this.right.insert(key, value);
@@ -42,7 +46,7 @@ class BinarySearchTree {
   //all operations are used using keys, values don't play much of a role unless you want to retrieve it
   find(key) {
     //if the key is in root, return the value
-    if(this.key === key) {
+    if (this.key === key) {
       return this.value;
     }
     //if the key is less than the root, follow the left side
@@ -52,7 +56,7 @@ class BinarySearchTree {
     }
     //if key is greater than root, follow right side
     //if there is an existing right child, call the fn recursively until you find the key 
-    else if (key > this.key && this.right){
+    else if (key > this.key && this.right) {
       return this.right.find(key);
     }
     //you searched the tree, the item is not there
@@ -61,14 +65,15 @@ class BinarySearchTree {
     }
   }
 
-  _replaceWith(node) {
-    if (this.parent) {
-      if (this === this.parent.left) {
+  _replaceWith(node) { //1
+    if (this.parent) { //2
+      if (this === this.parent.left) { // 1 
         this.parent.left = node;
       } else if (this === this.parent.right) {
         this.parent.right = node;
-      } if (node) {
-        node.parent = this.parent;
+      }
+      if (node) {
+        node.parent = this.parent; //2
       }
     }
     else {
@@ -95,16 +100,41 @@ class BinarySearchTree {
   }
 
   remove(key) {
-    if(this.key === key) { //if item exist in the root
-      if (this.left && this.right) {  //if both left & right children exists
+    if (this.key === key) {
+      if (this.left && this.right) {
+        // choose a new root by finding the smallest one on the right
         const successor = this.right._findMin();
         this.key = successor.key;
         this.value = successor.value;
         successor.remove(successor.key);
       }
+      //If the node only has a left child, 
+      //then you replace the node with its left child.  
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      //And similarly if the node only has a right child 
+      //then you replace it with its right child.
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      //If the node has no children then
+      //simply remove it and any references to it 
+      //by calling "this._replaceWith(null)".
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
     }
   }
-  
 }
 
 //3,1,4,6,9,2,5,7
--- interview.js
+++ interview.js
@@ -1,5 +1,5 @@
+'use strict';
 /* Given a sorted array, write an algorithm that creates a binary search tree from the elements of the sorted array in O(n) runtime. the resulting tree will be height balanced.  */
-
 
 /* 
 input: [3, 5, 7, 9, 11, 13, 15]
@@ -10,7 +10,11 @@ start at the middle
      5   13
     / \   / \
    3  7  11 15
+
+  Assumptions: Are we given a default BST JS class? Can we use default JS fn?
+  We need to start in the middle, so you need to keep track of start and end
 */
+
 
 //1. Find the middle of the array, make that the root
 //2. Recursively do below until it meets base case:
@@ -19,3 +23,29 @@ start at the middle
 //Take the right side of the middle and then find its middle and make that the right child of the root
 //4. Return the tree
 
+const sortedArraytoBST = (arr, start=0, end=arr.length-1) => {
+  let middle = Math.floor((start+end)/2);
+  let bst = new BinarySearchTree(arr[middle]);
+
+  //attach the node of the existing node, with a new end point for the left side
+  /* 
+       9
+      /
+     5
+   /
+  3
+  */
+  bst.left = sortedArraytoBST(arr, start, middle-1);
+  bst.right = sortedArraytoBST(arr, middle+1, end);
+  /* 
+       9
+      / \      when you switch to the right side
+    5    13    take care of the left side first (13 & 11)
+   / \*  / \
+  3  7  11  15
+  */
+};
+
+//recursion is not costly at this point, BST data structure lends itself to use recursive 
+
+//any tree's runtime is log(n)
