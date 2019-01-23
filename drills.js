17d54225d5d14117c47eb5838c54fd2404d4517c
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-23 12:27:55 -0500

    BST class

---
+++ drills.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//hierarchy, parent-child relationship
+//parent has a max of 2 children
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null){
+    //what is the key? 
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(){
+
+  }
+
+  remove() {
+
+  }
+
+  //is this using key or value?
+  find() {
+
+  }
+}
